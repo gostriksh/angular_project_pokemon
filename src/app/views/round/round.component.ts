09886b8878ce07ac7af0f822f0f48f0fc3278fd8
Timothée <timothee.glowacki@gmail.com>
2019-03-27 14:13:27 +0100

    add battles logs

--- src/app/views/round/round.component.ts
+++ src/app/views/round/round.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {PokemonService} from '../../core/services/pokemon.service';
 import {IPokemon} from '../../core/interfaces/IPokemon';
-import {concat, first, map, switchAll, switchMap} from 'rxjs/operators';
+import {concat, filter, first, map, switchAll, switchMap} from 'rxjs/operators';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Observable, OperatorFunction} from "rxjs";
+import {Observable, OperatorFunction} from 'rxjs';
 
 @Component({
     selector: 'app-round',
@@ -13,6 +13,9 @@ import {Observable, OperatorFunction} from "rxjs";
 export class RoundComponent implements OnInit {
     public pokemonFront: IPokemon;
     public pokemonBack: IPokemon;
+    public logs: string[] = [];
+    public winner: IPokemon;
+    public loser: IPokemon;
 
     constructor(private pokemonService: PokemonService,
                 private route: ActivatedRoute) {
@@ -20,7 +23,9 @@ export class RoundComponent implements OnInit {
 
     ngOnInit() {
         this.route.paramMap.pipe(
-            switchMap(this.fetchPokemons.bind(this))
+            switchMap(this.fetchPokemons.bind(this)),
+            filter(() => !!(this.pokemonFront && this.pokemonBack)),
+            first(this.fight.bind(this))
         )
             .subscribe();
     }
@@ -47,7 +52,42 @@ export class RoundComponent implements OnInit {
     }
 
     private fight(): void {
-        const pokemons = this.getAttackOrder(this.pokemonFront, this.pokemonBack)
+        let count = 0;
+        const pokemons = this.getAttackOrder(this.pokemonFront, this.pokemonBack);
+
+        const interval = setInterval(() => {
+            if (pokemons[0].stats.health <= 0 || pokemons[1].stats.health <= 0) {
+                clearInterval(interval);
+            }
+
+            const firstIndex = count % 2;
+            const secondIndex = (1 + count) % 2;
+
+            this.attack(pokemons[firstIndex], pokemons[secondIndex]);
+
+            if (pokemons[secondIndex].stats.health <= 0) {
+                this.winner = pokemons[firstIndex];
+                this.loser = pokemons[secondIndex];
+            } else {
+                count++;
+            }
+        }, 1000);
         return;
+    }
+
+    private attack(attacker: IPokemon, attacked: IPokemon) {
+        if (attacked.stats.health <= 0) {
+            return;
+        }
+
+        const damage: number = attacker.stats.attack - attacked.stats.defense;
+        const trueDamage: number = damage > 0 ? damage : 1;
+        attacked.stats.health -= trueDamage;
+
+        this.logs.push(`${attacker.name} attack ${attacked.name} and deal ${trueDamage}`);
+
+        if (attacked.stats.health < 0) {
+            attacked.stats.health = 0;
+        }
     }
 }
